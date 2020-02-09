7205128770f196dc599becedeb8e7631cf59eefd
John Santiago Jr <jdsantiagojr@protonmail.com>
2020-02-08 19:04:48 -0500

    change cli require

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs').promises
 const path = require('path')
-const parse = require('json-to-schema')
+const parse = require('@jdsantiagojr/json-to-schema')
 const { argv } = require('yargs')
   .option('file', {
     alias: 'f',
