56da7afea1b0cd5dd9b80a3771811b956b77f71c
John Santiago Jr <jdsantiagojr@protonmail.com>
2020-03-03 23:04:22 -0500

    Updated code. Trying something different.

--- index.js
+++ index.js
@@ -6,16 +6,30 @@ const hasOwnProperty = function (key, obj) {
   return Object.prototype.hasOwnProperty.call(obj, key)
 }
 
-const pipe = function (...args) {
-  return (x) => args.reduce((v, f) => f(v), x)
-}
-
 const omit = function (arr, obj) {
   return Object.keys(obj).filter(a => !arr.includes(a)).reduce((p, c) => ({ ...p, [`${c}`]: obj[c] }), {})
 }
 
 const pick = function (arr, obj) {
   return Object.keys(obj).filter(a => arr.includes(a)).reduce((p, c) => ({ ...p, [`${c}`]: obj[c] }), {})
+}
+
+const isArray = function (a) {
+  return Array.isArray(a)
+}
+
+const isRegExp = function (str) {
+  let b = true
+  try {
+    new RegExp(str)
+  } catch (e) {
+    b = false
+  }
+  return b
+}
+
+const pipe = function (...args) {
+  return (x) => args.reduce((v, f) => f(v), x)
 }
 
 const pass = function (a) {
@@ -32,33 +46,26 @@ const isValidType = (function () {
     'boolean',
     'null'
   ]
-  return (a) => (
-    Array.isArray(a) ? a.every(b => xs.includes(b)) : xs.includes(a)
-  )
+  return (a) => xs.includes(a)
 }())
 
 const inferType = function (a) {
-  return (a = isPlainObject(a) ? (a.type || a) : a) && !a ? 'string' : isValidType(a) ? a : (Array.isArray(a) ? 'array' : typeof a)
+  return (a = isPlainObject(a) && a.type || a) && !a ? 'string' : !isArray(a) && isValidType(a) ? a : isArray(a) ? 'array' : typeof a
 }
 
 const inferFormat = (function () {
   const xs = [
     ['email', /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/],
-    // ['idn-email'],
-    // ['regex'],
+    ['regex', { test: isRegExp }],
     ['ipv4', /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/],
     ['ipv6', /(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/],
     ['uri', /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/],
-    // ['uri-reference'],
-    // ['iri'],
-    // ['iri-reference'],
-    ['date-time', /^(\d{4})(?:-([0]\d|[1][0-2]))(?:-([0-2]\d|[3][01]))(?:T([01]\d|2[0-3]))(?::([0-5]\d))(?::([0-5]\d)(?:\.(\d{1,7}?)|)|)(Z|([+-])([01]\d|2[0-3])(?::([0-5]\d)))$/], // 2018-11-13T20:20:39+00:00
-    ['time', /(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/], // 20:20:39+00:00
-    ['date', /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/], // 2018-11-13
-    // ['hostname',],
-    // ['idn-hostname'],
-    // ['json-pointer']
-    // ['relative-json-pointer']
+    ['date-time', /^\d\d\d\d-[0-1]\d-[0-3]\d[t\s](?:[0-2]\d:[0-5]\d:[0-5]\d|23:59:60)(?:\.\d+)?(?:z|[+-]\d\d(?::?\d\d)?)$/i], // 2018-11-13T20:20:39+00:00
+    ['time', /^(?:[0-2]\d:[0-5]\d:[0-5]\d|23:59:60)(?:\.\d+)?(?:z|[+-]\d\d(?::?\d\d)?)?$/i], // 20:20:39+00:00
+    ['date', /^\d\d\d\d-[0-1]\d-[0-3]\d$/], // 2018-11-13
+    ['json-pointer', /^(?:\/(?:[^~/]|~0|~1)*)*$/],
+    ['relative-json-pointer', /^(?:0|[1-9][0-9]*)(?:#|(?:\/(?:[^~/]|~0|~1)*)*)$/],
+    ['uuid', /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i]
   ]
   return (a) => (
     typeof a === 'string' ? (xs.find(([_, regexp]) => regexp.test(a)) || [])[0] : a
@@ -118,123 +125,16 @@ const getkeywords = (function () {
   )
 }())
 
-const getExample = function (a) {
-  const x = isPlainObject(a) ? (a.examples || a.contains || a.enum) : a
-  return (
-    Array.isArray(x) ? x : [x]
-  )
+const useDefault = function (v) {
+  return v ? (a) => ({ ...a, default: (a.type = (a.type === 'integer') ? 'number' : a.type) && globalThis[`${a.type.charAt(0).toUpperCase()}${a.type.slice(1)}`]() }) : pass
 }
 
-const newObjectSchema = function (json, next = pass) {
-  const type = 'object'
-  const keywords = getkeywords(type)
-  const obj = json.type === type ? { ...(json.properties || {}), ...omit(['type', 'properties', ...keywords], json) } : { ...json }
-  const schema = {
-    ...Object.keys(obj)
-      .reduce((prev, key) => {
-        const value = newSchema(obj[key], next)
-
-        if (key.charAt(0) === '*') {
-          prev.required = [...prev.required, (key = key.substr(1))]
-        }
-
-        prev.properties = { ...prev.properties, [`${key}`]: value }
-
-        return prev
-      }, {
-        properties: {},
-        required: []
-      }),
-    ...pick(keywords, json),
-    type
-  }
-  return next(schema)
+const useDetectFormat = function (v) {
+  return v ? (a) => a.type === 'string' ? { ...a,  format: inferFormat(a._v) } : a : pass
 }
 
-const isHomogeneous = function (arr) {
-  const [head, tail] = [arr[0], arr.slice(1)]
-  const type = inferType(head)
-
-  return tail.every(a => inferType(a) === type)
-}
-
-const isValidItemType = function (type) {
-  return type === 'number' || type === 'string' || type === 'object'
-}
-
-const newArraySchema = function (json, next = pass) {
-  const type = 'array'
-  const keywords = getkeywords(type)
-  const xs = isPlainObject(json) && json.type === 'array' ? json.items : json
-  const schema = { type }
-
-  let items
-
-  if (Array.isArray(xs) && xs.length >= 1) {
-    if (isHomogeneous(xs)) {
-      items = newSchema(xs[0])
-    } else {
-      items = xs.map((a) => newSchema(a, next))
-    }
-  } else {
-    if (isValidItemType(inferType(xs))) {
-      items = newSchema(xs, next)
-    }
-  }
-
-  if (isPlainObject(items)) {
-    items.examples = xs
-  }
-
-  return next({ ...schema, ...pick(keywords, json), items })
-}
-
-const newGenericSchema = function (json, next = pass) {
-  const type = inferType(json)
-  const schema = {
-    ...pick(getkeywords(type), json),
-    type,
-    examples: getExample(json)
-  }
-
-  if (schema.type === 'string') {
-    const format = inferFormat(json)
-
-    if (!schema.format && format) {
-      schema.format = format
-    }
-  }
-
-  return next(schema)
-}
-
-const newSchema = function newSchema (json, next = pass) {
-  let type
-
-  if (isPlainObject(json)) {
-    if (isValidType(json.type)) {
-      type = json.type
-    }
-  }
-
-  if (!type) {
-    type = inferType(json)
-  }
-
-  let fn
-
-  if (type === 'object') {
-    fn = newObjectSchema
-  } else if (type === 'array') {
-    fn = newArraySchema
-  } else {
-    fn = newGenericSchema
-  }
-  return fn(json, next)
-}
-
-const useExamples = function (v) {
-  return v !== true ? (a) => omit(['examples'], a) : pass
+const useAdditionalProperties = function (v) {
+  return (a) => a.type === 'object' ? ({ ...a, additionalProperties: (a.additionalProperties || v) }) : a
 }
 
 const usePath = function (v) {
@@ -260,35 +160,112 @@ const usePath = function (v) {
   return v ? (a) => fn('', a) : pass
 }
 
-const useAdditionalProperties = function (v = true) {
-  return (a) => a.type === 'object' ? ({ ...a, additionalProperties: (a.additionalProperties || v) }) : a
+const useExample = function (v) {
+  return v ? (a) => ({...a, examples: a.type === 'array' ? isArray(a._v) ? a._v : [a._v] : a.examples || a.contains || a.enum || [a._v]}) : pass
 }
 
 const useRequired = function (v) {
   return v ? (a) => (a.type === 'object') ? { ...a, required: [...new Set([...Object.keys(a.properties), ...a.required])] } : a : pass
 }
 
-const useDetectFormat = function (v) {
-  return v ? pass : pass
+const useOutput = function (a) {
+  return omit(['_v'], a)
 }
 
-const useDefault = function (v) {
-  return v ? (a) => ({ ...a, default: (a.type = (a.type === 'integer') ? 'number' : a.type) && globalThis[`${a.type.charAt(0).toUpperCase()}${a.type.slice(1)}`]() }) : pass
+const newObjectSchema = function (json, next = pass) {
+  const type = 'object'
+  const keywords = getkeywords(type)
+  const obj = json.type === type ? { ...(json.properties || {}), ...omit(['type', 'properties', ...keywords], json) } : json
+  const schema = Object.keys(obj)
+    .reduce((prev, key) => {
+      const value = parse(obj[key], next)
+
+      if (key.charAt(0) === '*') {
+        prev.required = [...prev.required, (key = key.substr(1))]
+      }
+
+      prev.properties = { ...prev.properties, [`${key}`]: value }
+
+      return prev
+    }, {
+      type,
+      properties: {},
+      required: [],
+      _v: json,
+      ...pick(keywords, json)
+    })
+  return next(schema)
 }
 
-const parse = function (json, opt = {}) {
-  const schema = newSchema(json, pipe(...[
-    useAdditionalProperties(opt.additionalProperties),
-    usePath(opt.paths),
-    useRequired(opt.required),
-    useDefault(opt.defaults),
-    useExamples(opt.examples),
-    useDetectFormat(opt.detectFormat)
-  ]))
+const isHomogeneous = function (arr) {
+  const [head, tail] = [arr[0], arr.slice(1)]
+  const type = inferType(head)
 
-  if (opt.draft) {
-    schema.$schema = 'http://json-schema.org/draft-07/schema'
+  return tail.every(a => inferType(a) === type)
+}
+
+const isValidItemType = function (type) {
+  return type === 'number' || type === 'string' || type === 'object'
+}
+
+const newArraySchema = function (json, next = pass) {
+  const type = 'array'
+  const keywords = getkeywords(type)
+  const xs = isPlainObject(json) && json.type === 'array' ? json.items : json
+
+  let items
+
+  if (Array.isArray(xs) && xs.length >= 1) {
+    if (isHomogeneous(xs)) {
+      items = parse(xs[0], next)
+    } else {
+      items = xs.map((a) => parse(a, next))
+    }
+  } else {
+    if (isValidItemType(inferType(xs))) {
+      items = parse(xs, next)
+    }
   }
+  return next({ ...pick(keywords, json), type, items, _v: xs })
+}
+
+const newGenericSchema = function (json, next = pass) {
+  const type = inferType(json)
+  const schema = {
+    ...pick(getkeywords(type), json),
+    type,
+    _v: json
+  }
+  return next(schema)
+}
+
+const parse = function parse(json, next = pass) {
+  const type = inferType(json)
+
+  let fn
+
+  if (type === 'object') {
+    fn = newObjectSchema
+  } else if (type === 'array') {
+    fn = newArraySchema
+  } else {
+    fn = newGenericSchema
+  }
+
+  return fn(json, next)
+}
+
+const main = function (json, opt) {
+  const fn = pipe(
+    useRequired(opt.required),
+    useAdditionalProperties(opt.additionalProperties),
+    useDefault(opt.defaults),
+    usePath(opt.paths),
+    useDetectFormat(opt.detectFormat),
+    useExample(opt.examples),
+    useOutput
+  )
+  const schema = omit(['examples', 'default'], parse(json, fn))
 
   if (opt.id) {
     schema.$id = opt.id
@@ -302,9 +279,9 @@ const parse = function (json, opt = {}) {
 }
 
 if (typeof module !== 'undefined' && !module.parent) {
-  module.exports = parse
+  module.exports = main
 }
 
 if (typeof window !== 'undefined' && typeof window === 'object') {
-  window.jsonToSchema = parse;
+  window.jsonToSchema = main;
 }
